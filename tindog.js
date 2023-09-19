c1bee37caa07da1b6ab6b1be4b15b68a17a3f0d7
AskandaMahajan <107852586+AskandaMahajan@users.noreply.github.com>
2023-09-19 19:07:35 +0530

    Add files via upload

--- tindog.js
+++ tindog.js
@@ -9,11 +9,8 @@ const nav = document.querySelector('.nav');
 btnScrollTo.addEventListener('click', function (e) {
     const s1coords = section1.getBoundingClientRect();
     console.log(s1coords);
-  
     console.log(e.target.getBoundingClientRect());
-  
     console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-  
     console.log(
       'height/width viewport',
       document.documentElement.clientHeight,
@@ -22,24 +19,38 @@ btnScrollTo.addEventListener('click', function (e) {
     section1.scrollIntoView({ behavior: 'smooth' });
   });
 
-  
+///////////////////////////////////////
+//CHATBOT
   document.addEventListener('DOMContentLoaded', function () {
     const chatIcon = document.getElementById('chat-icon');
     const chatBox = document.getElementById('chat-box');
-  
+    const chatMessages = document.getElementById('chat-messages');
+    const userInput = document.getElementById('user-input');
     let chatBoxVisible = false; // Track the chatbox visibility
-  
     chatIcon.addEventListener('click', function () {
-      if (chatBoxVisible) {
-        chatBox.style.display = 'none'; // Hide the chatbox
-      } else {
-        chatBox.style.display = 'block'; // Show the chatbox
-      }
-  
-      // Toggle the chatbox visibility
-      chatBoxVisible = !chatBoxVisible;
+        if (chatBoxVisible) {
+            chatBox.style.display = 'none'; // Hide the chatbox
+        } else {
+            chatBox.style.display = 'block'; // Show the chatbox
+        }
+        // Toggle the chatbox visibility
+        chatBoxVisible = !chatBoxVisible;
     });
-  
-    // Rest of your chat functionality goes here
-  });
-  
+    userInput.addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') {
+            sendMessage(userInput.value);
+        }
+    });
+    function sendMessage(message) {
+        const userMessage = document.createElement('div');
+        userMessage.className = 'user-message';
+        userMessage.textContent = message;
+        chatMessages.appendChild(userMessage);
+        // Simulate a response from the chatbot (you can replace this with actual chatbot logic)
+        const botMessage = document.createElement('div');
+        botMessage.className = 'bot-message';
+        botMessage.textContent = 'Chatbot: Thanks for your message!';
+        chatMessages.appendChild(botMessage);
+        userInput.value = '';
+    }
+});
